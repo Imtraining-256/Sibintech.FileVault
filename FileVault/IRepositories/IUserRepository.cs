d08110cb2fcedc2dddf4ddc3634fde086b3dbb03
Атеняев Егор <atenyaev@protonmail.com>
2020-10-07 17:26:52 +0700

    added UserRepo

--- FileVault/IRepositories/IUserRepository.cs
+++ FileVault/IRepositories/IUserRepository.cs
@@ -4,8 +4,11 @@ namespace FileVault.IRepositories
 {
     interface IUserRepository
     {
-        public void AddUser();
+        public void AddUser(User user);
 
-        public User FindUser();
+        public User FindUser(User id);
+
+        public void Delete(int id);
+        public void Save();
     }
 }
---
+++ FileVault/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using FileVault.DAL;
+using FileVault.DAL.Entities;
+using FileVault.IRepositories;
+
+namespace FileVault.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private VaultFileContext _db;
+        public UserRepository()
+        {
+            _db = new VaultFileContext();
+        }
+
+        public void AddUser(User user)
+        {
+            _db.Users.Add(user);
+        }
+
+        public User FindUser(User id)
+        {
+            return _db.Users.Find(id);
+        }
+
+        public void Delete(int id)
+        {
+            var user = _db.Users.Find(id);
+
+            if (user != null)
+            {
+                _db.Users.Remove(user);
+            }
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
