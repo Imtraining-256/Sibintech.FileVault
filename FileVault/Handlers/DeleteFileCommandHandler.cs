9f60c92d2cdfb5cf753738481863fad5653085f9
Атеняев Егор <atenyaev@protonmail.com>
2020-10-10 17:12:59 +0700

    add delete file command

---
+++ FileVault/Handlers/DeleteFileCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FileVault.DAL;
+using FileVault.DAL.Entities;
+using FileVault.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileVault.Handlers
+{
+    public class DeleteFileCommandHandler : IRequestHandler<DeleteFileCommand, UploadFile>
+    {
+        private readonly VaultFileContext _dbContext;
+
+        public DeleteFileCommandHandler(VaultFileContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public async Task<UploadFile> Handle(DeleteFileCommand request, CancellationToken cancellationToken)
+        {
+            var file = await _dbContext.UploadFiles.FirstOrDefaultAsync(r => r.Id == request.Id);
+
+            if (file != null)
+            {
+                _dbContext.UploadFiles.Remove(file);
+            }
+
+            return file;
+        }
+    }
+}
---
+++ FileVault/Queries/DeleteFileCommand.cs
@@ -0,0 +1,15 @@
+﻿using FileVault.DAL.Entities;
+using MediatR;
+
+namespace FileVault.Queries
+{
+    public class DeleteFileCommand : IRequest<UploadFile>
+    {
+        public DeleteFileCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+}
