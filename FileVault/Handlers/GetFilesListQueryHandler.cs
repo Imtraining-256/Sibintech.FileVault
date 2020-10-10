989b89bc5c0dde0e2f80983c51554915bcdf303b
Атеняев Егор <atenyaev@protonmail.com>
2020-10-10 21:34:41 +0700

    added query user files

---
+++ FileVault/Handlers/GetFilesListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using FileVault.Queries;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FileVault.DAL;
+using FileVault.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileVault.Handlers
+{
+    public class GetFilesListQueryHandler : IRequestHandler<GetFilesListQuery, ICollection<UploadFile>>
+    {
+        private readonly VaultFileContext _db;
+
+        public GetFilesListQueryHandler(VaultFileContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ICollection<UploadFile>> Handle(GetFilesListQuery request, CancellationToken cancellationToken)
+        {
+            return await _db.UploadFiles.Where(r => r.User.Name == request.UserName).ToListAsync();
+        }
+    }
+}
---
+++ FileVault/Queries/GetFilesListQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using FileVault.DAL.Entities;
+
+namespace FileVault.Queries
+{
+    public class GetFilesListQuery : IRequest<ICollection<UploadFile>>, IRequest<Unit>
+    {
+        public GetFilesListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; private set; }
+    }
+}
