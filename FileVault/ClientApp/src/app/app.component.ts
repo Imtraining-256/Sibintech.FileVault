b061e8831910e1f2e05ee33235ebfec7f36c3e79
Атеняев Егор <atenyaev@protonmail.com>
2020-10-13 14:19:54 +0700

    FileService part

--- FileVault/ClientApp/src/app/app.component.ts
+++ FileVault/ClientApp/src/app/app.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileService } from './services/fileService.service';
+import { UploadFile } from "./models/uploadFile"
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [FileService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  uploadFile: UploadFile = new UploadFile();
+  uploadFiles: UploadFile[];
+  tableModel: boolean = true;
+
+  constructor(private fileService: FileService) {  }
+
+  ngOnInit(): void {
+     this.loadFilesList();
+  }
+
+  loadFilesList() {
+     this.fileService.getFilesList().subscribe((data: UploadFile[]) => this.uploadFiles = data);
+  }
+
+  upload() {
+
+  }
+
+  download() {
+
+  }
+
+  deleteFile() {
+
+  }
+
+
 }
--- FileVault/ClientApp/src/app/models/uploadFile.ts
+++ FileVault/ClientApp/src/app/models/uploadFile.ts
@@ -1,7 +1,7 @@
 export class UploadFile {
   constructor(
-    public id: number,
-    public userId: number,
-    public uploadDate: Date,
-    public fileName: string) { }
+    public id?: number,
+    public userId?: number,
+    public uploadDate?: Date,
+    public fileName?: string) { }
 }
