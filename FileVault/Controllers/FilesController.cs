90edacede2633549fb340905e4b6830e5c8b6dbc
Атеняев Егор <atenyaev@protonmail.com>
2020-10-15 18:03:51 +0700

    controller code cleanup

--- FileVault/Controllers/FilesController.cs
+++ FileVault/Controllers/FilesController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using FileVault.DAL.Entities;
+﻿using FileVault.Extensions;
 using FileVault.Queries;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FileVault.Extensions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
